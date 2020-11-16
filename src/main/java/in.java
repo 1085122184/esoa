a32317ac15ca47efe37e87f9f01b6b3bd24200a4
1085122184 <1085122184@qq.com>
2020-11-16 15:14:07 +0800

    test

--- src/main/java/in.java
+++ src/main/java/in.java
@@ -3,6 +3,6 @@ public class in {
         System.out.println(8500*0.8-722.5);//idea修改
         System.out.println("idea添加");
         System.out.println("giswww页面添加");
-
+        System.out.println("idea提交");
     }
 }
