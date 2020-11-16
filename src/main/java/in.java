06b7e9272c456c939871a59c0086b6111ca5bb87
1085122184 <1085122184@qq.com>
2020-11-16 14:53:39 +0800

    test

--- src/main/java/in.java
+++ src/main/java/in.java
@@ -1,7 +1,8 @@
 public class in {
     public static void main(String[] args) {
         System.out.println(8500*0.8-722.5);//idea修改
-System.out.println("git页面添加");
+        System.out.println("idea添加");
+
 
     }
 }
