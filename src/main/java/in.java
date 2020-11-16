816b0660eb477e95ef1246c78140f55eeb293ecc
1085122184 <1085122184@qq.com>
2020-11-16 15:22:29 +0800

    111

--- src/main/java/in.java
+++ src/main/java/in.java
@@ -3,11 +3,6 @@ public class in {
         System.out.println(8500*0.8-722.5);//idea修改
         System.out.println("idea添加");
         System.out.println("giswww页面添加");
-<<<<<<< HEAD
-        System.out.println("eclipse页面添加");
-
-=======
-        System.out.println("idea提交");
->>>>>>> branch 'master' of https://github.com/1085122184/esoa
+        System.out.println("eclipse页�添加");
     }
 }
