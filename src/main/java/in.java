276e3a051b6839f733efc422a8433cb849f957d5
1085122184 <42396352+1085122184@users.noreply.github.com>
2020-11-16 14:54:51 +0800

    Update in.java

--- src/main/java/in.java
+++ src/main/java/in.java
@@ -2,7 +2,7 @@ public class in {
     public static void main(String[] args) {
         System.out.println(8500*0.8-722.5);//idea修改
         System.out.println("idea添加");
-
+        System.out.println("git页面添加");
 
     }
 }
