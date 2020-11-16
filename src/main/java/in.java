90ca58fbb459c518508ffb464438bf3477d2b48c
1085122184 <1085122184@qq.com>
2020-11-16 15:31:03 +0800

    test

--- src/main/java/in.java
+++ src/main/java/in.java
@@ -1,7 +1,9 @@
 public class in {
     public static void main(String[] args) {
- System.out.println(8500*0.8-722.5);//idea修改
- System.out.println("idea添加");
- System.out.println("giswww页面添加");
+        System.out.println(8500*0.8-722.5);//idea修改
+        System.out.println("idea添加");
+        System.out.println("giswww页面添加");
+        System.out.println(12334);
+        //只是测试例子
     }
 }
