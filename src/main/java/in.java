2aaf4d4f146dfb419564ca48c10fb9e1436116a0
1085122184 <1085122184@qq.com>
2020-11-16 15:52:23 +0800

    杀杀杀

--- src/main/java/in.java
+++ src/main/java/in.java
@@ -1,10 +1,10 @@
 public class in {
     public static void main(String[] args) {
-        System.out.println(8500*0.8-722.5);//idea修改
-        System.out.println("idea添加");
-        System.out.println("git页面修改");
+        System.out.println(8500*0.8-722.5);//idea淇敼
+        System.out.println("idea娣诲姞");
+        System.out.println("git椤甸潰淇敼");
         System.out.println(12334);
-        //只是测试例子
-        System.out.println("idea新添加");
+        //鍙槸娴嬭瘯渚嬪瓙
+        System.out.println("eclipse鏂版坊鍔�");
     }
 }
