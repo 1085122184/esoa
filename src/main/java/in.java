f24e6d83e8e28f945ac3b1ef280196d95b20fc61
1085122184 <1085122184@qq.com>
2020-11-16 15:13:41 +0800

    eclipse 提交

--- src/main/java/in.java
+++ src/main/java/in.java
@@ -3,6 +3,7 @@ public class in {
         System.out.println(8500*0.8-722.5);//idea修改
         System.out.println("idea添加");
         System.out.println("giswww页面添加");
+        System.out.println("eclipse页面添加");
 
     }
 }
